510f85a8b1535729c7d904e8ec48ef601c763220
Zou Guangxian <zouguangxian@gmail.com>
2023-05-18 21:54:09 +0800

    fix: docker-compose version

--- images/linux/scripts/installers/docker-compose.sh
+++ images/linux/scripts/installers/docker-compose.sh
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Install docker-compose v1 from releases
-URL="https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64"
+URL="https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-Linux-$(uname -m)"
 curl -L $URL -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
