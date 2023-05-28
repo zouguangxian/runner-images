26e541b2499455f7743e6cb9f7a482b98da48d9a
Zou Guangxian <zouguangxian@gmail.com>
2023-05-28 11:40:51 +0800

    fix: NO_PUBKEY B53DC80D13EDEF05
    https://github.com/GoogleCloudPlatform/gcsfuse/issues/961#issuecomment-1445711531

--- images/linux/scripts/installers/google-cloud-sdk.sh
+++ images/linux/scripts/installers/google-cloud-sdk.sh
@@ -8,7 +8,7 @@ REPO_URL="https://packages.cloud.google.com/apt"
 
 # Install the Google Cloud SDK
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
-wget -q https://packages.cloud.google.com/apt/doc/apt-key.gpg -O /usr/share/keyrings/cloud.google.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get update -y
 apt-get install -y google-cloud-sdk
 
